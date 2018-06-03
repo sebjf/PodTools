b8f4beacf10ffd95f216c01fe64ab6e5f81fbd67
Ray Koopa <ray@syroot.com>
2018-06-03 14:06:22 +0200

    Convert texture pixel data.

--- src/io_scene_bl4/bl4.py
+++ src/io_scene_bl4/bl4.py
@@ -1,56 +1,69 @@
+from typing import *
 from . import pbdf
 from .binary import *
-from typing import *
 
 
 class Circuit:
     def __init__(self):
-        self.events = None
-        self.macros = None
-        self.track_name = None
-        self.texture_lod = None
-        self.project_name = None
-        self.textures = None
-        self.sectors = None
+        self.events = None  # type: List[Event]
+        self.macros = None  # type: MacroList
+        self.track_name = None  # type: str
+        self.texture_lod = None  # type: List[int]
+        self.project_name = None  # type: str
+        self.textures = None  # type: TextureList
+        self.sectors = None  # type: SectorList
 
     @classmethod
     def load(cls, file, key, block_size):
         new = cls.__new__(cls)
+
         offsets = pbdf.read_header_offsets(file, block_size)
         file.seek(offsets[0])
-        # Header
+
         if read_int32(file) != 3:
             raise AssertionError("Check value is not 3")
         read_int32(file)
-        # Events
+
         event_count = read_int32(file)
         _ = read_int32(file)  # Buffer size
         new.events = [Event.load(file) for _ in range(event_count)]
-        # Macros
+
         new.macros = MacroList.load(file)
-        # General
         new.track_name = pbdf.read_string(file)
         new.texture_lod = read_int32s(file, 16)
         new.project_name = pbdf.read_string(file)
-        # Textures
         new.textures = TextureList.load(file, 256, 2)
-        # Sectors
         new.sectors = SectorList.load(file, key)
         return new
 
 
 class Event:
+    def __init__(self):
+        self.name = None  # type: str
+        self.param_size = 0  # type: int
+        self.param_count = 0  # type: int
+        self.param_data = None  # type: bytes
+
     @classmethod
     def load(cls, file):
         new = cls.__new__(cls)
         new.name = pbdf.read_string(file)
-        new.siz_param = read_int32(file)
-        new.num_param = read_int32(file)
-        new.param_data = file.read(new.siz_param * new.num_param)
+        new.param_size = read_int32(file)
+        new.param_count = read_int32(file)
+        new.param_data = file.read(new.param_size * new.param_count)
         return new
 
 
 class MacroList:
+    def __init__(self):
+        self.base = None  # type: List[Macro]
+        self.main = None  # type: List[Macro]
+        self.init = None  # type: List[Macro]
+        self.active = None  # type: List[Macro]
+        self.desactive = None  # type: List[Macro]
+        self.remplace = None  # type: List[Macro]
+        self.echange = None  # type: List[Macro]
+
     @classmethod
     def load(cls, file):
         new = cls.__new__(cls)
@@ -65,6 +78,9 @@ class MacroList:
 
 
 class Macro:
+    def __init__(self):
+        self.values = None  # type: List[int]
+
     @classmethod
     def load(cls, file, value_count):
         new = cls.__new__(cls)
@@ -73,18 +89,32 @@ class Macro:
 
 
 class TextureList:
+    def __init__(self):
+        self._textures = []  # type: List[Texture]
+        self.size = 0  # type: int
+        self.pixel_size = 0  # type: int
+
+    def __iter__(self):
+        return self._textures.__iter__()
+
     @classmethod
     def load(cls, file, texture_size, pixel_size):
         new = cls.__new__(cls)
+        new.size = texture_size
+        new.pixel_size = pixel_size
         count = read_int32(file)
         _ = read_int32(file)
-        new.textures = [Texture.load(file) for _ in range(count)]
-        for texture in new.textures:
+        new._textures = [Texture.load(file) for _ in range(count)]
+        for texture in new._textures:
             texture.data = file.read(texture_size * texture_size * pixel_size)
         return new
 
 
 class Texture:
+    def __init__(self):
+        self.regions = []  # type: List[TextureRegion]
+        self.data = None  # type: bytes
+
     @classmethod
     def load(cls, file):
         new = cls.__new__(cls)
@@ -94,6 +124,14 @@ class Texture:
 
 
 class TextureRegion:
+    def __init__(self):
+        self.name = None  # type: str
+        self.left = 0  # type: int
+        self.top = 0  # type: int
+        self.right = 0  # type: int
+        self.bottom = 0  # type: int
+        self.index = 0  # type: int
+
     @classmethod
     def load(cls, file):
         new = cls.__new__(cls)
@@ -108,26 +146,26 @@ class TextureRegion:
 
 class SectorList:
     def __init__(self):
-        self.has_named_faces = False
-        self.sectors = []
+        self.has_named_faces = False  # type: bool
+        self._sectors = []  # type: List[Sector]
 
     def __iter__(self):
-        return self.sectors.__iter__()
+        return self._sectors.__iter__()
 
     @classmethod
     def load(cls, file, key):
         new = cls.__new__(cls)
         has_named_faces = read_int32(file) != 0
-        new.sectors = [Sector.load(file, key, has_named_faces) for _ in range(read_int32(file))]
+        new._sectors = [Sector.load(file, key, has_named_faces) for _ in range(read_int32(file))]
         return new
 
 
 class Sector:
     def __init__(self):
         self.mesh = None  # type: Mesh
-        self.vertex_lights = None
-        self.bounding_box_min = None
-        self.bounding_box_max = None
+        self.vertex_lights = None  # type: bytes
+        self.bounding_box_min = (0.0, 0.0, 0.0)  # type: Tuple(float, float, float)
+        self.bounding_box_max = (0.0, 0.0, 0.0)  # type: Tuple(float, float, float)
 
     @classmethod
     def load(cls, file, key, has_named_faces):
@@ -166,14 +204,14 @@ class MeshFace:
         self.vertex_count = None  # type: int
         self.normal = None  # type: tuple(float, float, float)
         self.material_name = None  # type: str
-        self.face_color = None
-        self.texture_index = None
-        self.texture_uvs = None
-        self.reserved = None
-        self.quad_reserved = None
-        self.unk_prop = None
-        self.properties = None
-        self.unk_reserved = None
+        self.face_color = None  # type: int
+        self.texture_index = None  # type: int
+        self.texture_uvs = None  # type: List[Tuple(float, float, float)]
+        self.reserved = None  # type: int
+        self.quad_reserved = None  # type: Tuple(float, float, float)
+        self.unk_prop = None  # type: int
+        self.properties = None  # type: int
+        self.unk_reserved = None  # type: int
 
     @classmethod
     def load(cls, file, key, has_name, has_unk_prop):
--- src/io_scene_bl4/importing.py
+++ src/io_scene_bl4/importing.py
@@ -23,6 +23,9 @@ class ImportOperator(bpy.types.Operator, bpy_extras.io_utils.ImportHelper):
         description="Filepath used for importing the BL4 file",
         maxlen=1024)
 
+    def __init__(self):
+        self.circuit = None  # type: bl4.Circuit
+
     def execute(self, context):
         file_name = self.properties.filepath
         file_size = os.path.getsize(file_name)
@@ -34,40 +37,45 @@ class ImportOperator(bpy.types.Operator, bpy_extras.io_utils.ImportHelper):
                 f.seek(0)
                 pbdf.decrypt(f, dec_file, key, block_size)
                 dec_file.seek(0)
-                circuit = bl4.Circuit.load(dec_file, key, block_size)
-        self.convert(circuit)
+                self.circuit = bl4.Circuit.load(dec_file, key, block_size)
+        self.convert()
         return {'FINISHED'}
 
-    def convert(self, circuit: bl4.Circuit):
-        for i, sector in enumerate(circuit.sectors):
+    def convert(self):
+        for i, texture in enumerate(self.circuit.textures):
+            self.convert_texture(texture, "Image{0:03d}".format(i), self.circuit.textures.size)
+        for i, sector in enumerate(self.circuit.sectors):
             self.convert_sector(sector, "Sector{0:03d}".format(i))
 
-    def convert_textures(self):
-        pass
+    def convert_texture(self, texture: bl4.Texture, name: str, size: int):
+        # Create image and convert upside-down RGB565 pixel data.
+        img = bpy.data.images.new(name, width=size, height=size)
+        pixels = [0.0] * size * size * 4
+        data = memoryview(texture.data).cast("H")
+        for y in range(size):
+            for x in range(size):
+                pixel = data[y * size + x]
+                idx = ((size + ~y) * size + x) * 4
+                pixels[idx] = (pixel >> 11) / 0b11111
+                pixels[idx + 1] = (pixel >> 5 & 0b111111) / 0b111111
+                pixels[idx + 2] = (pixel & 0b11111) / 0b11111
+                pixels[idx + 3] = 1.0
+        img.pixels = pixels
+
 
     def convert_sector(self, sector: bl4.Sector, name: str):
         bm = bmesh.new()
-
         # Add vertex positions (TODO: use normals with normals_split_custom_set()).
         for position in sector.mesh.positions:
             bm.verts.new(position)
         bm.verts.ensure_lookup_table()
         bm.verts.index_update()
-
-        # Create faces (provided as triangle list). Ignore duplicate faces for now (what's the deal with them?)
+        # Create faces (provided as triangle / quad list). Ignore duplicate faces for now (apparently not required).
         for face in sector.mesh.faces:
-            if face.vertex_count == 3:
-                verts = (bm.verts[face.indices[0]], bm.verts[face.indices[1]], bm.verts[face.indices[2]])
-            elif face.vertex_count == 4:
-                verts = (bm.verts[face.indices[0]], bm.verts[face.indices[1]], bm.verts[face.indices[2]], bm.verts[face.indices[3]])
-            else:
-                raise ValueError("Unsupported number of face vertices.")
             try:
-                bm.faces.new(verts)
+                bm.faces.new((bm.verts[face.indices[i]] for i in range(face.vertex_count)))
             except ValueError as e:
                 print(e)
-
-
         # Link the BMesh to a mesh object in the scene.
         mesh = bpy.data.meshes.new(name)
         bm.to_mesh(mesh)
